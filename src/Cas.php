cf1ed763e80b74464f17eb0652781aab19d26efd
zhangyubo <zhangyubo@gouuse.cn>
2019-11-14 18:49:48 +0800

    初始化

--- src/Cas.php
+++ src/Cas.php
@@ -332,10 +332,10 @@ class Cas
 
     public function getURL()
     {
-        $final_uri = ($this->_isHttps()) ? 'https' : 'http';
-        $final_uri .= '://';
+        //$final_uri = ($this->_isHttps()) ? 'https' : 'http';
+        //$final_uri .= '://';
 
-        $final_uri .= $this->_client_url;//$this->_getClientUrl();
+        $final_uri = $this->_client_url;//$this->_getClientUrl();
         $request_uri	= explode('?', $this->_SERVER['REQUEST_URI'], 2);
         $final_uri		.= $request_uri[0];
 
