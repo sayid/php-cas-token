f769ef3e1fad67063e380399f26e3ed0341cbf56
zhangyubo <zhangyubo@gouuse.cn>
2020-06-19 18:20:32 +0800

    打断点

--- src/Cas.php
+++ src/Cas.php
@@ -124,7 +124,6 @@ class Cas
         ];
         $response = $client->request('POST', $validate_url, $form_params);
         $text_response = $response->getBody()->getContents();
-
         $dom = new \DOMDocument();
         // Fix possible whitspace problems
         $dom->preserveWhiteSpace = false;
@@ -133,6 +132,7 @@ class Cas
         // read the response of the CAS server into a DOMDocument object
         if ( !($dom->loadXML($text_response))) {
             // read failed
+            getGouuse()->LogLib->debug($text_response, [], false, "cas_response");
             throw new \Exception(
                 'Ticket not validated'. $validate_url
             );
